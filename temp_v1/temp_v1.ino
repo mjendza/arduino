a33c7cb9b6d384241512ed9172bdcd799a00dae3
totoo <jendzuch@gmail.com>
2015-04-23 22:32:15 +0200

    #13 HC-06 bluetooth communication - v1

--- temp_v1/temp_v1.ino
+++ temp_v1/temp_v1.ino
@@ -1,104 +1,46 @@
-#include <DHT.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <EtherCard.h>
+#include <IRremote.h>
+#include <SoftwareSerial.h>
+//http://www.arduino.cc/en/Reference/SoftwareSerial
+int RECV_PIN = 5;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
-// Data wire is plugged into pin 7 on the Arduino
-#define ONE_WIRE_BUS 7
-// ethernet interface mac address, must be unique on the LAN
-static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
-static byte myip[] = { 192,168,1,149 };
+SoftwareSerial mySerial(10, 11); // RX, TX
 
-byte Ethernet::buffer[500];
-BufferFiller bfill;
-
-// Setup a oneWire instance to communicate with ANY OneWire devices 
-OneWire oneWire(ONE_WIRE_BUS);
-
-// Set oneWire reference to Dallas Temperature sensor. 
-DallasTemperature sensors(&oneWire);
-
-float h =0.0;
-float f = 0.0; 
-double t_dt20b18=20.0;
-
-#define DHTTYPE DHT11 
-#define DHTPIN 2 
-DHT dht(DHTPIN, DHTTYPE);
-
-
-void setup(void)
+void setup()
 {
-  //DT20B18
   Serial.begin(9600);
-  sensors.begin();
-  //DTH
-  //dht.begin();
+ 
+  irrecv.enableIRIn(); // Start the receiver
+  digitalWrite(6, HIGH);
+  //digitalWrite(12, HIGH);  
+  pinMode(6, OUTPUT);     
+  //pinMode(12, OUTPUT);
   
-  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
-      Serial.println( "Failed to access Ethernet controller");
-  ether.staticSetup(myip);  
+    // set the data rate for the SoftwareSerial port
+  mySerial.begin(4800);
+  mySerial.println("Hello, world?");
+  
 }
 
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    //FF6897 button "1"
 
-void loop(void)
-{
-  //readDht();
-  readTemp();
-  setHomePage();
-//temp++;
-//if(temp>8000){
-  //temp=0;   
+    irrecv.resume(); // Receive the next value
+  }
+  delay(4000);
+  digitalWrite(6, HIGH); // dioda na Arduino
+  //digitalWrite(12, HIGH); // przekaźnik
+  delay(4000);
+  digitalWrite(6, LOW);
+  //digitalWrite(12, LOW);
   
-}
-void setHomePage()
-{
-  //ethernet
- word len = ether.packetReceive();
-  word pos = ether.packetLoop(len);
-  
-  if (pos)  // check if valid tcp data is received
-    ether.httpServerReply(homePage()); // send web page data
-}
-void readDht(){
-  //h = dht.readHumidity();
-  // Read temperature as Celsius
-  f = dht.readTemperature();
-  
-  
-  Serial.println("temp"); 
-  Serial.println(f); 
-  //Serial.println(""); 
-  //Serial.println("hum"); 
-  //Serial.println(h); 
-}
-void readTemp()
-{
- sensors.requestTemperatures();
-delay(500);
-t_dt20b18 = sensors.getTempCByIndex(0); // first founded sensor have Index 0 
-Serial.println(t_dt20b18); 
+//   if (mySerial.available())
+//    Serial.write(mySerial.read());
+//  if (Serial.available())
+//    mySerial.write(Serial.read());
 }
 
-
-static word homePage() {
-  long t = millis() / 1000;
-  word h = t / 3600;
-  byte m = (t / 60) % 60;
-  byte s = t % 60;
-  bfill = ether.tcpOffset();
-  char buffera[10];
-  bfill.emit_p(PSTR(
-    "HTTP/1.0 200 OK\r\n"
-    "Content-Type: text/html\r\n"
-    "Pragma: no-cache\r\n"
-    "\r\n"
-    "<meta http-equiv='refresh' content='1'/>"
-    "<title>Arduino server</title>"
-    "<h1>$D$D:$D$D:$D$D</h1>"
-    "<h1>temp $S</h1>"
-    ),
-      h/10, h%10, m/10, m%10, s/10, s%10, dtostrf(t_dt20b18, 2, 2, buffera) );//dtostrf(t_dt20b18, 2, 2, buffera)
-  return bfill.position();
-}
 
